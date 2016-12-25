d2bd00ec46f98690f92b7182f97d50e021c06f29
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-12-25 19:01:47 +0530

    User account management completed

--- imports/ui/components/navbar/navbar.js
+++ imports/ui/components/navbar/navbar.js
@@ -1,1 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import './navbar.html';
+
+Template.Navbar.helpers({
+	currentUserEmail() {
+		return Meteor.user().emails[0].address;
+	}
+});
+
+Template.Navbar.events({
+	'click .logout'(event) {
+		event.preventDefault();
+
+		Meteor.logout();
+
+		FlowRouter.go('login');
+	}
+});
--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,1 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import './home.html';
+
+Template.Home.events({
+	'click .btn-lg'(event) {
+		event.preventDefault();
+
+		FlowRouter.go('login');
+	}
+});
